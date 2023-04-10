4bdd89b3c50f4aa02283f514831ec72caa45418f
lucky_2206 <vaibhavjainlucky98@gmail.com>
2023-04-10 22:34:03 +0530

    Routing and reading file using express

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
-//How to make a server
-import http from "http";
-import {calc} from './features.js'
-import { readFileSync } from "fs";
+import express from 'express';
+import path from 'path';
 
-//Reading a file we have to use fs module 
-// We will call read function syncronously
-const home = readFileSync('./index.html')
-const server = http.createServer((req,res)=>{
-    if(req.url==='/about')
-        res.end(`<h1>${calc()} is your Love Percentage</h1>`);    
-    else if(req.url==='/')
-        res.end(home);
-    else if(req.url==='/contact')
-        res.end("<h1>Contact Page</h1>");
-    else
-        res.end("<h1>Page Not Found</h1>");
+const app = express();
+app.get("/",(req,res)=>{
+    //to access a file we do the following steps
+    // res.send("HIII");
+    const pathlocation = path.resolve();
+    res.sendFile(path.join(pathlocation,"./index.html"));
 });
-
-server.listen(5000,()=>{
-    console.log("server is working properly");
+app.listen(5000,()=>{
+    console.log("Server is working fine");
 })
