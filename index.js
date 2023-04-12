c262bf4e94626b3d96de8eadcf78d212f9396432
lucky_2206 <vaibhavjainlucky98@gmail.com>
2023-04-12 23:55:25 +0530

    Registeration form using EJS and connecting the api using the array

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const app = express();
 //the below line is used when we are statically using a file
 app.use(express.static(path.join(path.resolve(),"public")));
 
+//using middleware
+app.use(express.urlencoded({extended:true}));
 //setting up view engine
 app.set("view engine","ejs");
 
-
+const user=[];
 /*{Eihter we can first set up the view engine using the above command and then pass only index 
 or we can direclty write index.ejs}*/
 
@@ -17,9 +19,27 @@ or we can direclty write index.ejs}*/
 
 
 app.get("/",(req,res)=>{
-    // res.render("index",{name:"Vaibhav"});
-    res.send(index.html);
+    res.render("index");
+    
 });
-app.listen(5000,()=>{ 
-    console.log("Server is working fine");
+
+app.get("/success",(req,res)=>{
+    res.render("success");
+    
+});
+app.post("/contact",(req,res)=>{
+    user.push({userName:req.body.name,email:req.body.email});
+    
+    //instes of redirectiing it and then making another get for success we can direclty render it
+    // res.render("success");
+    res.redirect("/success");
+})
+
+// here we are considering our array as api can get the data by visiting the url http://localhost:5000/users
+app.get("/users",(req,res)=>{
+    res.json({user});
+})
+const port = 5000;
+app.listen(port,()=>{ 
+    console.log("Server is working fine on port", port);
 })
